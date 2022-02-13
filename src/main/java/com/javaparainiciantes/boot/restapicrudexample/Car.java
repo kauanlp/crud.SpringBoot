2c115e8a76bcd17d50bd5a44e7496a3a5642cb01
Kauan <kauan-linhares@hotmail.com>
2022-02-13 14:13:06 -0300

    primeira versao

---
+++ src/main/java/com/javaparainiciantes/boot/restapicrudexample/Car.java
@@ -0,0 +1,19 @@
+package com.javaparainiciantes.boot.restapicrudexample;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data; 
+
+// o Car vai ser uma "Entity" do JPA porque ele vai ser salvo no banco
+
+@Entity
+@Data
+@AllArgsConstructor // construtor com todos os parametros
+@
+public class Car {
+    Long id;
+    String brand;
+    String model;
+    Integer year;
+}
